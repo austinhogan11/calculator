95772088181842629bd9b4ca6bc41aaecb8ece7d
Austin Hogan <austinhogandev@gmail.com>
2023-09-08 15:52:53 -0400

    Debugging why not chaining operations

--- calculate.js
+++ calculate.js
@@ -22,6 +22,13 @@ let onFirstNumber = true;
 let onSecondNumber = false;
 let currentNumber = 0;
 
+const displayTxt = document.querySelector(".display-txt");
+const numBtns = document.querySelectorAll(".num-btn");
+const decimalBtn = document.querySelector(".decimal");
+const opBtns = document.querySelectorAll(".op-btn");
+const clearBtn = document.querySelector('#clear-btn');
+const equalBtn = document.querySelector('#eq-btn');
+
 function operate(firstNumber, operator, secondNumber) {
     switch (operator) {
         case "+":
@@ -29,7 +36,6 @@ function operate(firstNumber, operator, secondNumber) {
         case "-":
             return formatDecimals(subtract(firstNumber, secondNumber));
         case "×":
-            console.log(operator)
             return formatDecimals(multiply(firstNumber, secondNumber));
         case "÷":
             if (secondNumber !== 0) {
@@ -44,47 +50,49 @@ function operate(firstNumber, operator, secondNumber) {
     }
 }
 
-const displayTxt = document.querySelector(".display-txt");
-const numBtns = document.querySelectorAll(".num-btn");
-numBtns.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-        if (buffer.length < 9) {
-            if (onFirstNumber) {
-                firstNumber = buildCurrentNumber(btn);
-            } else {
-                secondNumber = buildCurrentNumber(btn);
-            }
-        }
-    });
-});
-
 function buildCurrentNumber(btn) {
-    buffer.push(btn.textContent);
-    console.log(`Buffer: ${buffer}`);
-    currentNumber = parseFloat(buffer.join(''));
-    console.log(`firstNumber: ${currentNumber}`);
-    displayTxt.textContent = currentNumber;
-    return currentNumber;
+    if (buffer.length < 9) {
+        buffer.push(btn.textContent);
+        console.log(`Buffer: ${buffer}`)
+        currentNumber = parseFloat(buffer.join(''));
+        console.log(`Current Number: ${currentNumber}`)
+        displayTxt.textContent = currentNumber;
+        return currentNumber;
+    }
 }
 
-const decimalBtn = document.querySelector(".decimal");
-decimalBtn.addEventListener('click', () => {
-    if (!buffer.includes(".")) {
-        buffer.push(decimalBtn.textContent);
-        console.log(`Buffer: ${buffer}`);
-        if (onFirstNumber) {
-            firstNumber = parseFloat(buffer.join(''));
-            console.log(`firstNumber: ${firstNumber}`);
-            displayTxt.textContent = firstNumber;
-        } else {
-            secondNumber = parseFloat(buffer.join(''));
-            console.log(`secondNumber: ${secondNumber}`);
-            displayTxt.textContent = secondNumber;
-        }
-    }
-});
+function clearCalculator() {
+    displayTxt.textContent = "0";
+    buffer = [];
+    firstNumber = undefined;
+    secondNumber = undefined;
+    currentNumber = 0;
+    operator = undefined;
+    onFirstNumber = true;
+    onSecondNumber = false;
+    console.log(`Cleared Calculator`)
+    console.log(`First Number: ${firstNumber} Second Number: ${firstNumber} Current Number: ${currentNumber} Operator: ${operator}`)
+}
 
-const opBtns = document.querySelectorAll(".op-btn");
+function processEquals() {
+    if (secondNumber !== undefined) {
+        result = operate(parseFloat(firstNumber), operator, parseFloat(secondNumber));
+        console.log(`Processed Equals - Result: ${result}`);
+        clearCalculator();
+        displayTxt.textContent = result;
+        firstNumber = result;
+        console.log(`First Number: ${firstNumber} Second Number: ${firstNumber} Current Number: ${currentNumber} Operator: ${operator}`)
+
+    }
+}
+
+function formatDecimals(num) {
+    return parseFloat(num.toString().slice(0, 9));
+}
+
+clearBtn.addEventListener('click', clearCalculator);
+equalBtn.addEventListener('click', processEquals);
+
 opBtns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
         console.log(btn.textContent);
@@ -93,49 +101,37 @@ opBtns.forEach((btn) => {
             operator = btn.textContent;
             onFirstNumber = false;
             onSecondNumber = true;
+            console.log(`Operator: ${operator}`);
+            console.log("On second number");
         } else {
             processEquals();
             operator = btn.textContent;
+            console.log(`Operator: ${operator}`);
+            console.log("On second number");
         }
     });
 });
 
-function clearCalculator() {
-    displayTxt.textContent = "0";
-    buffer = [];
-    firstNumber = undefined;
-    secondNumber = undefined;
-    operator = undefined;
-    onFirstNumber = true;
-    onSecondNumber = false;
-}
-
-const clearBtn = document.querySelector('#clear-btn');
-clearBtn.addEventListener('click', clearCalculator);
-
-function processEquals() {
-    if (secondNumber !== undefined) {
-        result = operate(parseFloat(firstNumber), operator, parseFloat(secondNumber));
-        clearCalculator();
-        displayTxt.textContent = result;
-        firstNumber = result;
+decimalBtn.addEventListener('click', () => {
+    if (!buffer.includes(".")) {
+        if (onFirstNumber) {
+            firstNumber = buildCurrentNumber(decimalBtn);
+            console.log(`First Number: ${firstNumber}`);
+        } else {
+            secondNumber = buildCurrentNumber(decimalBtn)
+            console.log(`Second Number: ${secondNumber}`);
+        }
     }
-}
+});
 
-const equalBtn = document.querySelector('#eq-btn');
-equalBtn.addEventListener('click', processEquals);
-
-// const delBtn = document.querySelector('#del-btn');
-// delBtn.addEventListener('click', () => {
-//     buffer.pop();
-// });
-
-function formatDecimals(num) {
-    return parseFloat(num.toString().slice(0, 9));
-}
-
-// const pmBtn = document.querySelector('#pm-btn');
-// pmBtn.addEventListener('click', changeSign);
-
-// function changeSign() {}
-
+numBtns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        if (onFirstNumber) {
+            firstNumber = buildCurrentNumber(btn);
+            console.log(`First Number: ${firstNumber}`);
+        } else {
+            secondNumber = buildCurrentNumber(btn);
+            console.log(`Second Number: ${secondNumber}`);
+        }
+    });
+});
